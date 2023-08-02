ea5e48db9169a495ba2266629392abbc4cd60ed3
aniket singh <aniket.singh@iot83.com>
2023-08-02 17:48:36 +0530

    api integrated and reducer created

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection';
-// import { useProductContext } from './Context/productcontext';
+// import { useProductContext } from './Context/productContext';
 
 const About = () => {
 	// const { myName } = useProductContext();
--- src/Context/productcontext.js
+++ src/Context/productcontext.js
@@ -1,41 +1,44 @@
-/*-we need to create three things to use context 
-	it has three components 
-	1.context -- similar to shopkeeper
-	2.provider -- similar to delivery boy
-	3.consumer -- similar to consumer (particular pages act as a consumer) 
--*/
-
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-
+import reducer from "../Reducer/ProductReducer";
 
 const AppContext = createContext();
+
 const API = "https://api.pujakaitem.com/api/products";
 
-const AppProvider = ({children}) => {
+const initialState = {
+	isLoading: false,
+	isError: false,
+	products: [],
+	featureProducts: [],
+};
+
+const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
 
 	const getProducts = async (url) => {
-		const res = await axios.get(url);
-		const products = await res.data;
-		console.log(res)
-		console.log("products:",products);
+		dispatch({ type: "SET_LOADING" });
+		try {
+			const res = await axios.get(url);
+			const products = await res.data;
+			dispatch({ type: "SET_API_DATA", payload: products });
+		} catch (error) {
+			dispatch({ type: "API_ERROR" });
+		}
 	};
 
-	useEffect(()=>{
+	useEffect(() => {
 		getProducts(API);
-	},[])
+	}, []);
 
 	return (
-		<AppContext.Provider value={{myName: "Aniket Singh"}}>
-			{children}
-		</AppContext.Provider>
-	)
-}
+		<AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+	);
+};
 
-//custom hooks for particular page
-
-const useProductContext = () =>{
+// custom hooks
+const useProductContext = () => {
 	return useContext(AppContext);
 };
 
-export {AppProvider, AppContext, useProductContext};
+export { AppProvider, AppContext, useProductContext };
