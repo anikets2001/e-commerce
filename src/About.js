c5c93f60b9fbc47337c247d060dd27ce4de500fc
aniket singh <aniket.singh@iot83.com>
2023-08-02 17:59:01 +0530

    error resolved

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection';
-// import { useProductContext } from './Context/productContext';
+// import { useProductContext } from './Context/ProductContext';
 
 const About = () => {
 	// const { myName } = useProductContext();
